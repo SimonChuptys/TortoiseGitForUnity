761c86db3f93e36afba6179162b5973ee226d686
Simon Chuptys <simon.chuptys@vintecc.com>
2020-09-23 15:47:57 +0200

    Samples added

---
+++ Samples~/DefaultInitialization/Editor/TortoiseGitForUnityDefaultInitialization.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using Vintecc.TortoiseGitForUnity;
+
+[InitializeOnLoad]
+public class TortoiseGitForUnityDefaultInitialization
+{
+    private const string TortoiseGitForUnityEnableMenuPath = "Tools/Enable TortoiseGitForUnity";
+
+    // .. INITIALIZATION
+
+    static TortoiseGitForUnityDefaultInitialization()
+    {
+        TortoiseGitForUnityLoader.InitializeDefaults();
+    }
+
+    // .. OPERATIONS
+
+    [MenuItem(TortoiseGitForUnityEnableMenuPath)]
+    private static void ToggleTortoiseGitForUnity()
+    {
+        Menu.SetChecked(TortoiseGitForUnityEnableMenuPath, TortoiseGitForUnityLoader.ToggleEnabled());
+    }
+}
